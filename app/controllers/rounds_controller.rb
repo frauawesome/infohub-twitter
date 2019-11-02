75333cf5e9b430bc7f3cd930309564d28c4f59a6
Carly Schmitting <cschmitting@gmail.com>
2019-11-03 00:26:14 +0100

    starting it up

---
+++ app/controllers/rounds_controller.rb
@@ -0,0 +1,10 @@
+class RoundsController < ApplicationController
+  ROUNDS = {
+    1 => { name: "Dishoom", address: "Shoreditch, London", category: "indian" },
+    2 => { name: "Sushi Samba", address: "City, London", category: "japanese" }
+  }
+
+  def index
+    @rounds = ROUNDS
+  end
+end
